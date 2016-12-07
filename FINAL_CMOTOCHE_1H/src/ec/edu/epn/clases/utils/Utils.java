9f99711a459607f8242eca29d64e11d6fad28b3c
CristhianMotoche <cristhian.motoche@gmail.com>
2016-12-07 16:03:41 -0500

    Add utils

---
+++ FINAL_CMOTOCHE_1H/src/ec/edu/epn/clases/utils/Utils.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.epn.clases.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author camm
+ */
+public class Utils {
+    public static String formatDate(Date date){
+        return new SimpleDateFormat("MM/dd/yyyy").format(date);
+    }
+}
