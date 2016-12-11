33d93e011ac1069aa42d45c23277dd82faddd329
CristhianMotoche <cristhian.motoche@gmail.com>
2016-12-11 18:19:07 -0500

    Add more Javadoc

--- FINAL_CMOTOCHE_1H/src/ec/edu/epn/clases/controller/LoginController.java
+++ FINAL_CMOTOCHE_1H/src/ec/edu/epn/clases/controller/LoginController.java
@@ -33,6 +33,7 @@ public class LoginController
     private Usuario user;
 
     /**
+     * Empty constructor
      *
      */
     public LoginController() {}
--- FINAL_CMOTOCHE_1H/src/ec/edu/epn/clases/controller/MainMenuController.java
+++ FINAL_CMOTOCHE_1H/src/ec/edu/epn/clases/controller/MainMenuController.java
@@ -39,6 +39,8 @@ public class MainMenuController
 
     /**
      *
+     *  Empty constructor
+     * 
      */
     public MainMenuController() {
     }
